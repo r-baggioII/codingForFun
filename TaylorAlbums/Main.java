faedecde2430613fa92ff00928fb70a4e770f3eb
r-baggioII <167486553+r-baggioII@users.noreply.github.com>
2024-08-09 17:40:17 -0300

    update Main.java

--- TaylorAlbums/Main.java
+++ TaylorAlbums/Main.java
@@ -5,11 +5,13 @@ public class Main {
         // Create some songs with float durations
         Song loveStory = new Song("Love Story", 3.55f, "Fearless");
         Song youBelongWithMe = new Song("You Belong with Me", 3.51f, "Fearless");
+        Song fifteen = new Song("Fifteen", 4.54f, "Fearless");
 
         // Create an album and add songs to it
         Album fearless = new Album("Fearless", 2008);
         fearless.addSong(loveStory);
         fearless.addSong(youBelongWithMe);
+        fearless.addSong(fifteen);
 
         // Create an album library and add the album to it
         AlbumLibrary library = new AlbumLibrary();
