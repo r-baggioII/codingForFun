c6ae181f174b6cb0c4711db3ac244b2e9e675262
r-baggioII <167486553+r-baggioII@users.noreply.github.com>
2024-08-09 17:37:40 -0300

    bug fixes and updates

--- yety.py
+++ yety.py
@@ -1,151 +1,138 @@
-import random 
-#Ask the user to enter the game mode 
+import random
+
+# Ask the user to enter the game mode
 def selectGameMode():
     print('Select game mode:')
     print("1: 3x6 board")
-    print("2: 4x9 board") 
+    print("2: 4x9 board")
 
     gameMode = input()
-    while int(gameMode) != 1 and int(gameMode) != 2: 
-        print("Please, select one opction: 1 or 2")    
-        gameMode = input() 
-    return int(gameMode)  
+    while gameMode not in ['1', '2']:
+        print("Please, select one option: 1 or 2")
+        gameMode = input()
+    return int(gameMode)
 
-#Get initial score base onf the gameMode selected 
-def getInitialScore(gameMode): 
+# Get initial score based on the game mode selected
+def getInitialScore(gameMode):
     if gameMode == 1:
-        return 150 
-    else: 
-        return 300 
-    
-#Get initial board base on the gameMode selected    
-def getInitialBoard(gameMode): 
+        return 150
+    else:
+        return 300
+
+# Get initial board based on the game mode selected
+def getInitialBoard(gameMode):
     if gameMode == 1:
         board = [
-            [' ', ' ', ' ', ' ', ' ', ' '], 
-            [' ', ' ', ' ', ' ', ' ', ' '], 
+            [' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ']
         ]
-        return board
-    if gameMode ==2:  
+    elif gameMode == 2:
         board = [
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
         ]
-        return board
+    return board
 
-#Print the game board
-def printGameBoard(board): 
-    for row in board: 
-        print(row) 
+# Print the game board
+def printGameBoard(board):
+    for row in board:
+        print(row)
 
-def userPlay(gameMode): 
+# Get the row number from the user based on the game mode
+def userPlay(gameMode):
     userRow = int(input("Enter the row number: "))
-    if gameMode == 1: 
-        while userRow < 1 or userRow > 3: 
-            print("Please, select a valid row number: 1,2,3") 
-            userRow = int(input("Enter the row number: "))
-    else: 
-        while userRow < 1 or userRow > 4: 
-            print("Please, select a valid row number: 1,2,3,4") 
-            userRow = int(input("Enter the row number: "))
-    userRow = userRow - 1
-    return userRow 
+    max_row = 3 if gameMode == 1 else 4
+    while userRow < 1 or userRow > max_row:
+        print(f"Please, select a valid row number: 1-{max_row}")
+        userRow = int(input("Enter the row number: "))
+    return userRow - 1
 
-def randomPlay(gameMode): 
-    if gameMode == 1: 
-        randomRow = random.randint(0,2) 
-    else: 
-        randomRow = random.randint(0,3)
-    return randomRow 
+# Get a random row number based on the game mode
+def randomPlay(gameMode):
+    return random.randint(0, 2) if gameMode == 1 else random.randint(0, 3)
 
-def updateBoard(userRow,randomRow,board,currentColumn): 
-    if userRow == randomRow: 
+# Update the board after each play
+def updateBoard(userRow, randomRow, board, currentColumn):
+    if userRow == randomRow:
         board[randomRow][currentColumn] = 'X'
-    else: 
-        board[randomRow][currentColumn] = "2" 
+    else:
+        board[randomRow][currentColumn] = "2"
         board[userRow][currentColumn] = "1"
-    return board 
+    return board
 
-def checkState(gameMode,userRow,randomRow,currentColumn):
-    if userRow == randomRow: 
+# Check the current state of the game
+def checkState(gameMode, userRow, randomRow, currentColumn):
+    if userRow == randomRow:
         return "Game Over"
-    else: 
-        currentColumn = currentColumn + 1 
-        if gameMode == 1: 
-            if currentColumn >= 6: 
-                return "Win"
-            else:
-                return "Continue"
-        else: 
-            if currentColumn >= 9: 
-                return "Win"  
-            else:
-                return "Continue"   
+    currentColumn += 1
+    max_columns = 6 if gameMode == 1 else 9
+    return "Win" if currentColumn >= max_columns else "Continue"
 
-def updateScore(score,state): 
-    if state == "Game Over": 
-        score = score - 50 
-        return score
-    if state == "Continue": 
-        score = score + 20 
-        return score
+# Update the score based on the game state
+def updateScore(score, state):
+    if state == "Game Over":
+        score -= 50
+    elif state == "Continue":
+        score += 20
     return score
 
-def game(gameMode,score,board,currentColumn): 
-
-    userRow = userPlay(gameMode) 
-    randomRow = randomPlay(gameMode) 
-    board = updateBoard(userRow,randomRow,board,currentColumn) 
+# Main game loop
+def game(gameMode, score, board, currentColumn):
+    userRow = userPlay(gameMode)
+    randomRow = randomPlay(gameMode)
+    board = updateBoard(userRow, randomRow, board, currentColumn)
     printGameBoard(board)
-    state = checkState(gameMode,userRow,randomRow,currentColumn)
-    score = updateScore(score,state) 
+    state = checkState(gameMode, userRow, randomRow, currentColumn)
+    score = updateScore(score, state)
     return score, state
 
-def playAgain(): 
+# Check if the user wants to play again
+def playAgain():
     print("Do you want to play again? (yes/no)")
-    playAgain = input()
-    while playAgain != "yes" and playAgain != "no":
+    playAgainResponse = input().lower()
+    while playAgainResponse not in ["yes", "no"]:
         print("Please, enter a valid option: yes or no")
-        playAgain = input()
-    return playAgain
+        playAgainResponse = input().lower()
+    return playAgainResponse
 
+# Game setup
+gameMode = selectGameMode()
+print("Game mode selected:", gameMode)
+score = getInitialScore(gameMode)
+board = getInitialBoard(gameMode)
+printGameBoard(board)
+currentColumn = 0
+score, state = game(gameMode, score, board, currentColumn)
+playAgainResponse = "yes"
 
-gameMode = selectGameMode() 
-print("Game mode selected: ", gameMode)
-score = getInitialScore(gameMode) 
-board = getInitialBoard(gameMode) 
-printGameBoard(board) 
-currentColumn = 0 
-score,state = game(gameMode,score,board,currentColumn)
-playAgain = "yes" 
-
-while score > 0 and playAgain == "yes"and state != "Win": 
+# Game loop
+while score > 0 and playAgainResponse == "yes" and state != "Win":
     if state == "Game Over":
-        print("Game Over!") 
-        print("Your final score is: ", score)
-        playAgainUser = playAgain()
-        
-        if playAgain == "yes": 
-            board = getInitialBoard(gameMode) 
-            printGameBoard(board) 
-            currentColumn = 0 
-            score,state = game(gameMode,score,board,0)
+        print("Game Over!")
+        print("Your final score is:", score)
+        playAgainResponse = playAgain()
+
+        if playAgainResponse == "yes":
+            board = getInitialBoard(gameMode)
+            printGameBoard(board)
+            currentColumn = 0
+            score, state = game(gameMode, score, board, currentColumn)
         else:
-            print("Thanks for playing!") 
-            print("Your final score is: ", score)
+            print("Thanks for playing!")
+            print("Your final score is:", score)
     else:
-        print("Your current score is: ", score)
-        currentColumn = currentColumn + 1
-        score,state = game(gameMode,score,board,currentColumn)
+        print("Your current score is:", score)
+        currentColumn += 1
+        score, state = game(gameMode, score, board, currentColumn)
 
-if score < 0: 
+if score <= 0:
     print("You lost!")
-    print("Your final score is: ", score)
+    print("Your final score is:", score)
 
-if state == "Win": 
+if state == "Win":
     print("Congratulations! You win!")
-    print("Your final score is: ", score)
+    print("Your final score is:", score)
     print("Thanks for playing!")
